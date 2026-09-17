6e49368e996cdb7667333a4d0e062b14b12ba2d5
YoussefZahran2005 <youssef.assem.zahran@gmail.com>
2026-09-17 08:59:54 +0300

    added controllers, final for V 1.0

--- src/main/java/com/pl/premier_league_bro/controller/PlayerController.java
+++ src/main/java/com/pl/premier_league_bro/controller/PlayerController.java
@@ -1,10 +1,50 @@
 package com.pl.premier_league_bro.controller;
 
+import java.net.http.HttpResponse;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
-@RequestMapping(path = "api/v1/player")
-public class PlayerController {
+import com.pl.premier_league_bro.entity.Player;
+import com.pl.premier_league_bro.service.PlayerService;
 
+@RestController
+@RequestMapping(path = "/api/v1/player")
+public class PlayerController {
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Player>> getPlayers(
+            @RequestParam(required = false) String team,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String position,
+            @RequestParam(required = false) String nation) {
+        List<Player> players = playerService.searchPlayers(team, name, position, nation);
+        return new ResponseEntity<>(players, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player) {
+        Player savedPlayer = playerService.addPlayer(player);
+        return new ResponseEntity<>(savedPlayer, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Player> updatePlayer(@RequestBody Player player) {
+        Player updatedPlayer = playerService.updatePlayer(player);
+        return new ResponseEntity<>(updatedPlayer, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Void> deletePlayer(@RequestBody Player player) {
+        playerService.deletePlayer(player);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
